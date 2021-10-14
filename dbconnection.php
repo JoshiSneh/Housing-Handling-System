4157d4b10b21286a58d9558113bddfacc265839f
Sneh Joshi <snehj814@gmail.com>
2021-10-14 12:35:09 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "yavesu";
+$username = "remotemysql.com";
+$password = "GVBR9Zfoqi";
+$dbname = "hbXP1z10ci";
 
 $conn =  mysqli_connect($servername,$username,$password,$dbname);
 ?>
